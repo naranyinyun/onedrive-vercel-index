6445663b317212bc85ae8f99950fd35a03d5218e
纳兰音韵 <91950505+naranyinyun@users.noreply.github.com>
2023-08-10 00:51:20 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by onedrive-vercel-index.</br>All files can be shared or downloaded without any restrictions.</br>If  you need us deleted files, send email to <!--sse-->nalanyinyun@nalanyinyun.top<!--/sse-->  .',
+    'Powered by onedrive-vercel-index.</br>All files can be shared or downloaded without any restrictions.</br>If  you need us deleted files, send email to <!--sse-->nalanyinyun@nalanyinyun.top<!--/sse-->.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
